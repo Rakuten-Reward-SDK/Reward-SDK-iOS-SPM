c2642f51d7b07cf320d7323ede44c9a2dde50bf0
Derick <derick.derick@rakuten.com>
2024-08-26 16:27:40 +0800

    Update v11.2.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "RakutenRewardSDK",
             url: "https://github.com/Rakuten-Reward-SDK/Reward-SDK-iOS/releases/download/11.2.0/RakutenRewardSDK-11.2.0.xcframework.zip",
-            checksum: "c9db75b5d7973c8d4485c4718e3ee9e94cee6157d0a88eccd1a7c0fffefaddb6"
+            checksum: "28a77259ad371432f902abd0595663d01e3113ef79fdc73f0baf8e8104c1d401"
         ),
     ]
 )
